94bd3fabf4ffc1b1bb5d15342f5b7b17e4cf067f
Taehwan-Choi <choith89@gmail.com>
2022-03-08 17:25:16 +0900

    www

--- @Practice.py
+++ @Practice.py
@@ -1,1 +1,16 @@
-print('hello')
+import random
+
+test = [x for x in range(1,101)]
+
+random.shuffle(test)
+
+print(test)
+
+def bubble(a):
+    for k in range(0,len(a)):
+        for i in range(len(a)-1,k,-1):
+            if a[i-1]>a[i]:
+                a[i-1],a[i]=a[i],a[i-1]
+
+bubble(test)
+print(test)
